9256e426e4a4f423ffed21c3fc87123f8acf9dd3
MatheusIrlan <matheusibessa@hotmail.com>
2022-11-17 17:57:58 -0300

    Criacao de telas restantes

--- screens/screens.h
+++ screens/screens.h
@@ -1,6 +1,9 @@
 int exibirMenu();
-void exibir_remover_Candidato(Candidato *lista);
+void exibir_remover_Candidato(char *nome, int idade, int numero, char *partido, char *vice, char *estado);
 void exibirCabecarioListarCandidatos();
-void exibirListarCandidatos(Candidato **lista);
+void exibirListarCandidatos(char *nome, int idade, int numero, char *partido, char *vice, char *estado);
 void exibirBuscarCandidato(int num);
 void exibirCabecarioEditarCandidato(int num);
+void exibirEditarCandidato(char *nome, int idade, int numero, char *partido, char *vice, char *estado);
+void exibirLocalizacaoUrna(int num);
+void exibirAdicionarCandidato(char *nome, int idade, int numero, char *partido, char *vice, char *estado);
