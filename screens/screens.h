5a32c02117d652b108749e4f97aaea25393e803c
MatheusIrlan <matheusibessa@hotmail.com>
2022-11-17 17:19:45 -0300

    Criacao de telas de busca e edicao

--- screens/screens.h
+++ screens/screens.h
@@ -1,2 +1,6 @@
 int exibirMenu();
 void exibir_remover_Candidato(Candidato *lista);
+void exibirCabecarioListarCandidatos();
+void exibirListarCandidatos(Candidato **lista);
+void exibirBuscarCandidato(int num);
+void exibirCabecarioEditarCandidato(int num);
